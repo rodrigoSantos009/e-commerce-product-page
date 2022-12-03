4f5df5fdee931a3ff98d1893e860640a4d358857
Rodrigo <009rodrigo009@gmail.com>
2022-12-03 09:21:02 -0300

    Cart updated

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 import { formatCurrency } from "./formatCurrency.js";
 
-const numberItems = document.querySelector('.number-items')
-const quantityCart = document.querySelector('#quantity-cart')
+const numberItems = document.querySelector(".number-items");
+const quantityCart = document.querySelector("#quantity-cart");
 const openCart = document.querySelector("#open-cart");
 const cart = document.querySelector("#cart");
+const deleteItems = document.querySelector(".delete-items");
 let valueItem = document.querySelector(".value-item");
 const totalItems = document.querySelector(".total-items");
 const totalValue = document.querySelector(".total-value");
@@ -11,67 +12,63 @@ const minus = document.querySelector(".minus");
 const plus = document.querySelector(".plus");
 const btnAddCart = document.querySelector("#btn-add-cart");
 let quantityItem = document.querySelector(".quantity-item");
-quantityItem.value = 0
-let total = 0
+const emptyCart = document.querySelector(".empty-cart");
+const itemsCart = document.querySelector("#items");
+let total = 0;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const addTotalItems = () => {
-  formatCurrency()
-}
+let cartItems = false;
 
 const addToCart = () => {
-  valueItem = 125
-  totalItems.innerHTML = quantityItem.value
-  valueItem *= parseInt(quantityItem.value)
-  totalValue.innerHTML = formatCurrency(valueItem)
-  addNumberItems();
-}
+  if(quantityItem.value > 0) {
+    valueItem = 125;
+    totalItems.innerHTML = quantityItem.value;
+    valueItem *= parseInt(quantityItem.value);
+    totalValue.innerHTML = formatCurrency(valueItem);
+    addNumberItems();
+    showItems();
+  }
+};
+
 
 const addNumberItems = () => {
-  if(quantityItem.value > 0) {
+  if (quantityItem.value > 0) {
     quantityCart.style.display = "flex";
     numberItems.innerHTML = quantityItem.value;
-    
-  }
-}
+    cartItems = true;
+  } 
+};
 
-const clear = () => {
-  
-}
+const showItems = () => {
+  if (cartItems) {
+    emptyCart.style.display = "none";
+    itemsCart.style.display = "flex";
+  } 
+};
 
-btnAddCart.addEventListener('click', () => {
-  addToCart()
+const clear = () => {};
+
+deleteItems.addEventListener('click', () => {
+  emptyCart.style.display = "flex";
+  itemsCart.style.display = "none";
+  quantityCart.style.display = "none";
 })
 
+btnAddCart.addEventListener("click", () => {
+  addToCart();
+});
+
 openCart.addEventListener("click", () => {
-  cart.classList.toggle('open-cart')
+  cart.classList.toggle("open-cart");
 });
 
 minus.addEventListener("click", () => {
-  if(quantityItem.value > 0) {
+  if (quantityItem.value > 0) {
     total -= 1;
-    quantityItem.value = total
+    quantityItem.value = total;
   }
-})
+});
 
-plus.addEventListener('click', () => {
-  total += 1
-  quantityItem.value = total
-})
+plus.addEventListener("click", () => {
+  total += 1;
+  quantityItem.value = total;
+});
